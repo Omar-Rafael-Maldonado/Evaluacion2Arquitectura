fa9fb6f4d9ccdd3caec202b5d735b8c6fb0b4317
Omar_Maldonado <omarafmaljim@gmai.com>
2022-01-12 00:04:24 -0500

    Proyecto Evaluación Omar Maldonado

--- Motor_A_Pasos/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -o "C:/Users/omara/OneDrive/Escritorio/Examen/Motor_A_Pasos/Motor_A_Pasos_isim_beh.exe" -prj "C:/Users/omara/OneDrive/Escritorio/Examen/Motor_A_Pasos/Motor_A_Pasos_beh.prj" "work.Motor_A_Pasos" 
